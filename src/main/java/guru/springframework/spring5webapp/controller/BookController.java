48fe70e007b89234426458d6e4f9a2974ee6be0b
i-sokar <simosokar5@gmail.com>
2023-05-04 02:33:57 +0300

    book ctrl
    added book ctrl +active h2 db

---
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repos.BookRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepo repo;
+
+    public BookController(BookRepo repo) {
+        this.repo = repo;
+    }
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("Books",repo.findAll());
+        return"books";
+
+    }
+
+
+}
